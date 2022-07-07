24048cc6f6ec9daa0de70532add2960ec38d4006
Enzo Mateus Gonçalves <enzomateusgonc@gmail.com>
2022-07-07 19:29:05 -0300

    basic website

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'open-sans': ['"Open Sans"', 'ui-sans-serif', 'sans-serif'],
+    },
+    extend: {
+      screens: {
+        'xs': '400px',
+        '2xs': '300px'
+      },
+      boxShadow: {
+        'base': '0px 0px 15px rgb(0 0 0 / 6%)',
+      },
+      backgroundImage: {
+        'cta': "url('../src/assets/images/cta-bg.png')",
+        'footer': "url('../src/assets/images/footer-bg.png')",
+      },
+      gridTemplateColumns: {
+        'auto_2': 'repeat(2, max-content)',
+      },
+    },
+  },
+  plugins: [],
+}
